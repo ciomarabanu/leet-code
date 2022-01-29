130d23e05c33034ba9b7cff9e1e5d284a69b5d90
ciomarabanu <mara.mocanita@gmail.com>
2022-01-29 15:45:22 +0200

    add test for ClimbingStairs

--- src/main/java/com/github/ciomarabanu/leetcode/ClimbingStairs.java
+++ src/main/java/com/github/ciomarabanu/leetcode/ClimbingStairs.java
@@ -1,21 +1,13 @@
 package com.github.ciomarabanu.leetcode;
 
 public class ClimbingStairs {
-    public static void main(String[] args) {
-        System.out.println(climbStairs(5));
-    }
 
     public static int climbStairs(int n) {
-        if (n == 0) {
-            return 0;
-        }
-        if (n == 1) {
-            return 1;
-        }
+        if (n == 0) return 0;
+        if (n == 1) return 1;
 
         int oneBack = 2;
         int twoBack = 1;
-
 
         for (int i = 3; i <= n; i++) {
             int dummy = twoBack + oneBack;
---
+++ src/test/java/com/github/ciomarabanu/leetcode/ClimbingStairsTest.java
@@ -0,0 +1,13 @@
+package com.github.ciomarabanu.leetcode;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ClimbingStairsTest {
+
+    @Test
+    void test() {
+        Assertions.assertEquals(10946,  ClimbingStairs.climbStairs(20));
+    }
+
+}
