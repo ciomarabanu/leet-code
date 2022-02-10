69356e527d34b628dd6620bcce00ba455309d454
ciomarabanu <mara.mocanita@gmail.com>
2022-02-10 13:06:40 +0200

    add PowerOfTwo

---
+++ src/main/java/com/github/ciomarabanu/leetcode/PowerOfTwo.java
@@ -0,0 +1,13 @@
+package com.github.ciomarabanu.leetcode;
+
+public class PowerOfTwo {
+    public boolean isPowerOfTwo(int n) {
+        if (n == 1)
+            return true;
+
+        if (n % 2 != 0 || n == 0)
+            return false;
+
+            return isPowerOfTwo(n / 2);
+    }
+}
