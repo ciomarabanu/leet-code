be9d88046fd5308af783a80a5d652d923fef73e1
ciomarabanu <mara.mocanita@gmail.com>
2022-02-10 13:06:56 +0200

    add RemoveLinkedListElements

---
+++ src/main/java/com/github/ciomarabanu/leetcode/RemoveLinkedListElements.java
@@ -0,0 +1,12 @@
+package com.github.ciomarabanu.leetcode;
+
+public class RemoveLinkedListElements {
+    public ListNode removeElements(ListNode head, int val) {
+        if (head == null)
+            return null;
+
+        head.next = removeElements(head.next, val);
+        return head.val == val ? head.next : head;
+
+    }
+}
