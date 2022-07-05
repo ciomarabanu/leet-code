4f7eb4e0620d95e6d8732c434ab14129e3f5ee06
ciomarabanu <mara.mocanita@gmail.com>
2022-07-05 15:21:53 +0300

    add longestConsecutiveSeq (w/o sorting O(n))

--- src/main/java/com/github/ciomarabanu/leetcode/LongestConsecutiveSequence.java
+++ src/main/java/com/github/ciomarabanu/leetcode/LongestConsecutiveSequence.java
@@ -5,24 +5,45 @@ import java.util.stream.Collectors;
 
 public class LongestConsecutiveSequence {
     public static void main(String[] args) {
-        int[] nums = {1,2,0,1};
+        int[] nums = {100, 4, 200, 1, 3, 2};
         System.out.println(longestConsecutive(nums));
     }
+
     public static int longestConsecutive(int[] nums) {
         if (nums.length == 0) {
             return 0;
         }
-        int longestSeq = 1;
-        int currSeq = 1;
-        var a = Arrays.stream(nums).sorted().toArray();
-        for (int i = 1; i < nums.length; i++) {
-            if (a[i] == a[i - 1] + 1){
-                currSeq++;
-                longestSeq = Math.max(currSeq, longestSeq);
-            } else if (a[i] != a[i - 1]) {
-                currSeq = 1;
+        var maxSeq = 1;
+        var prevVal = 0;
+        var nextVal = 0;
+
+        var a = Arrays.stream(nums).boxed()
+            .collect(Collectors.toSet());
+
+        for (var num : nums) {
+            var seq = 1;
+            prevVal = num - 1;
+            nextVal = num + 1;
+            a.remove(num);
+
+            while (!a.isEmpty()) {
+
+                if (a.contains(prevVal)) {
+                    a.remove(prevVal);
+                    prevVal--;
+                    seq++;
+                }
+                if (a.contains(nextVal)) {
+                    a.remove(nextVal);
+                    nextVal++;
+                    seq++;
+                }
+                if (!a.contains(nextVal) && !a.contains(prevVal)) {
+                    maxSeq = Math.max(maxSeq, seq);
+                    break;
+                }
             }
         }
-        return longestSeq;
+        return maxSeq;
     }
 }
